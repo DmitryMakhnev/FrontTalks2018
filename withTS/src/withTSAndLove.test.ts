fa1588171f799673de549c6b01ce1ad55b163b10
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2018-05-10 01:31:47 +0300

    soundness

---
+++ withTS/src/withTSAndLove.test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert';
+
+import { Girl, Boy } from "./withTSAndLove";
+
+function printBoyData(boy: Boy) : string {
+    return `${boy.name} ${boy.age}`;
+}
+
+describe('withTSAndLove', () => {
+    
+    it('S as soundness', () => {
+        const girlInstance : Girl = new Girl('Sally', 21);
+
+        assert.strictEqual(
+            printBoyData(girlInstance),
+            'Sally 21'
+        );
+    });
+    
+});
